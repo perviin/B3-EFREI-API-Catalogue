c11975ebbf659295238b3473052b46d9c3aadff2
Pmaioranool <lucasmaiorano77@gmail.com>
2025-10-14 12:27:16 +0200

    ajout endpoint product

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+const Products = require("./models/ProductsModel");
 dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
+
 app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 
 app.get("/api/", (req, res) => res.send("Bienvenue sur mon API !"));
+
+app.route("/api/products").get(Products.getAll);
---
+++ models/ProductsModel.js
@@ -0,0 +1,9 @@
+const data = require("../data.json");
+class Products {
+  static getAll() {
+    console.log("za");
+    return data;
+  }
+}
+
+module.exports = Products;
